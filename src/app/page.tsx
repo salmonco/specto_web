4b73a38bf305e28acde6d9742b1928687bb101b5
salmonco <xkqjsslsek80@naver.com>
2024-06-18 19:30:51 +0900

    Design: main ing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p>스펙토 - 1:1 스펙 매니징 서비스</p>
-      <p>웹 사이트 공사중...</p>
+    <main className="flex min-h-screen flex-col gap-10 items-center justify-center p-24">
+      <Image
+        alt="logo"
+        src={require("../../public/images/logo.png")}
+        width={200}
+        height={200}
+        priority
+        className="rounded-lg"
+      />
+      <p className="text-[4rem] font-bold text-[#0094FF]">Specto</p>
+      <p className="text-[1.8rem] font-medium">
+        스펙 관리에 어려움을 겪는 대학생을 위한, 1:1 스펙 매니징 서비스
+      </p>
+      <div className="text-[1.4rem] flex flex-col items-center gap-4">
+        <p>
+          문의:{" "}
+          <Link href={"mailto:qhdwltn1380@gmail.com"}>
+            <span className="hover:underline hover:text-sky-500">
+              qhdwltn1380@gmail.com
+            </span>
+          </Link>
+        </p>
+        <Link href={"/privacy"}>
+          <span className="font-bold">개인정보처리방침</span>
+        </Link>
+      </div>
     </main>
   );
 }
